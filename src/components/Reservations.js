a4870e2c63e8208bd44c54315e7e0d3a8b10023c
[mahtot] <[mahtotgher13@gmail.com]>
2023-10-28 04:15:01 +0300

    added the table booking feature

---
+++ src/components/Reservations.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+
+function Reservations() {
+    const [reserve, setReserve] = useState([{
+                                   date: '',
+                                   time: '',
+                                   number_guests: '',
+                                   occasion:''
+    }]);
+    const availableTimes = ["17:00", "18:00", "19:00" , "20:00", "21:00", "22:00"]
+    const availableTimess = availableTimes.map(time=>{
+                         return <option value={time}>{time}</option>
+    })
+    const handleChange= (e)=>{
+        const {name, value} = e.target;
+        setReserve((prevReserve)=>({ ...prevReserve, [name]:value }))
+    }
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(reserve)
+    }
+   
+    return (
+    <div>
+        
+        <form 
+        style={{display: "grid",
+         gap: "20px", justifyContent:"center"}}
+         onSubmit={handleSubmit}>
+        
+        <label>
+            Choose date
+            <input type="date" 
+                   name="date"
+                   value={reserve.date}
+                   onChange={handleChange}
+                   />
+        </label>
+
+        <label>
+                Choose time
+         <select name="time"
+                 value={reserve.time}
+                 onChange={handleChange}>
+            {availableTimess}
+         </select>
+        </label>
+       <label>
+        Number of guests
+        <input type="number" placeholder="1"
+                min={1} max={10}
+                value={reserve.number_guests}
+                name="number_guests"
+                onChange={handleChange}/>
+       </label>
+
+       <label>Occasion
+            <select value={reserve.occasion}
+                    name="occasion"
+                    onChange={handleChange}>
+                <option value="Anniversary">Anniversary</option>
+                <option value="Birthday">Birthday</option>
+            </select>
+        </label>
+       <input type="submit" 
+              value="Make your reservation"
+              disabled={!reserve.date}/>
+        </form>
+
+
+
+    </div>
+  )
+}
+export default Reservations
