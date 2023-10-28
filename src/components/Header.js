73ec0291ea8d970ccdf246acb8d4499031783d4a
[mahtot] <[mahtotgher13@gmail.com]>
2023-10-28 04:13:50 +0300

    added the table booking feature

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,6 +6,9 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { solid, regular, light, thin, duotone, icon } from '@fortawesome/fontawesome-svg-core/import.macro' // <-- import styles to be used
 import Bars from '../assets/bars-solid.svg'
 import { useState } from 'react';
+import {Routes, Route, Link} from 'react-router-dom';
+import Main from './Main';
+import Reservations from './Reservations';
 function Header() {
 
   const [isPressed, setISPressed] = useState(false)
@@ -40,16 +43,20 @@ function Header() {
      
        <ul>
       
-            <li className="link"><a href="#">Home</a></li>
-            <li className="link"><a href="#">About</a></li>
+            <li className="link"><Link to='/'>Home</Link></li>
+            <li className="link"><Link to='/'>About</Link></li>
             <li className="link"><a href="#">Menu</a></li>
-            <li className="link"><a href="#">Reservations</a></li>
+            <li className="link"><Link to='/reservations'>Reservations</Link></li>
             <li className="link"><a href="#">Order Online</a></li>
             <li className="link"><a href="#">Login</a></li>
         </ul>
      </nav>
      </header>
-          
+
+  <Routes>
+     <Route path='/' element={<Main/>}/>
+     <Route path='/reservations' element={<Reservations/>} />
+  </Routes>
       
      
 {isPressed?
